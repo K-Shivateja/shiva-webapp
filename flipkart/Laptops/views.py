cd76091c58657d7a16d040e204dba4ec4395149e
K-Shivateja <konchelimishivateja@gmail.com>
2023-09-29 16:20:05 +0530

    first commit

---
+++ flipkart/Laptops/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Dell(request):
+    return render(request,'Dell.html')
+def Asus(request):
+    return render(request,'Asus.html')
---
+++ flipkart/Mobiles/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Realme(request):
+    return render(request,"Realme.html")
+
+def Redmi(request):
+    return render(request,"Redmi.html")
